8e7c93c6bac7de4dde9579d3653a6646119a18b4
Noham <72859612+Noh4m@users.noreply.github.com>
2023-12-13 15:00:10 +0100

    fix: image

--- front/src/components/Info/index.jsx
+++ front/src/components/Info/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './style.css'
+import Logoschneider from '../../assets/moyNPGA2G7F1DlDtFkpK.png'
 export default function Info() {
 return (
 <div className="flex flex-col gap-16 w-full h-[659px] items-start info">
     <div className="flex flex-row justify-between ml-4 w-full items-start">
-        <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image5" id="Image5" className="w-12" />
+        <img src={Logoschneider} alt="Image5" id="Image5" className="w-12" />
         <div className="flex flex-row gap-4 w-1/2 items-start">
             <div className="flex flex-col mt-2 gap-1 w-5/6 items-start">
                 <div id="IlYAMinutesAFP9" className="text-xl font-sans text-[#6b6d78]">
@@ -17,7 +18,7 @@ return (
                     arcu quis turpis ultrices elementum.{" "}
                 </div>
             </div>
-            <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image1" id="Image1" className="w-12" />
+            <img src={Logoschneider} alt="Image1" id="Image1" className="w-12" />
         </div>
         <div className="flex flex-col mt-2 gap-1 w-2/5 items-start">
             <div id="IlYAMinutesAFP1" className="text-xl font-sans text-[#6b6d78]">
@@ -32,7 +33,7 @@ return (
         </div>
     </div>
     <div className="flex flex-row justify-between ml-4 w-full items-start">
-        <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image6" id="Image6" className="w-12" />
+        <img src={Logoschneider} alt="Image6" id="Image6" className="w-12" />
         <div className="flex flex-row gap-4 w-1/2 items-start">
             <div className="flex flex-col mt-2 gap-1 w-5/6 items-start">
                 <div id="IlYAMinutesAFP11" className="text-xl font-sans text-[#6b6d78]">
@@ -45,7 +46,7 @@ return (
                     arcu quis turpis ultrices elementum.{" "}
                 </div>
             </div>
-            <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image2" id="Image2" className="w-12" />
+            <img src={Logoschneider} alt="Image2" id="Image2" className="w-12" />
         </div>
         <div className="flex flex-col mt-2 gap-1 w-2/5 items-start">
             <div id="IlYAMinutesAFP3" className="text-xl font-sans text-[#6b6d78]">
@@ -60,7 +61,7 @@ return (
         </div>
     </div>
     <div className="flex flex-row justify-between ml-4 w-full items-start">
-        <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image7" id="Image7" className="w-12" />
+        <img src={Logoschneider} alt="Image7" id="Image7" className="w-12" />
         <div className="flex flex-row gap-4 w-1/2 items-start">
             <div className="flex flex-col mt-2 gap-1 w-5/6 items-start">
                 <div id="IlYAMinutesAFP13" className="text-xl font-sans text-[#6b6d78]">
@@ -73,7 +74,7 @@ return (
                     arcu quis turpis ultrices elementum.{" "}
                 </div>
             </div>
-            <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image3" id="Image3" className="w-12" />
+            <img src={Logoschneider} alt="Image3" id="Image3" className="w-12" />
         </div>
         <div className="flex flex-col mt-2 gap-1 w-2/5 items-start">
             <div id="IlYAMinutesAFP5" className="text-xl font-sans text-[#6b6d78]">
@@ -88,7 +89,7 @@ return (
         </div>
     </div>
     <div className="flex flex-row justify-between w-full items-start mb-1 ml-4">
-        <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image8" id="Image8" className="w-12" />
+        <img src={Logoschneider} alt="Image8" id="Image8" className="w-12" />
         <div className="flex flex-row gap-4 w-1/2 items-start">
             <div className="flex flex-col mt-2 gap-1 w-5/6 items-start">
                 <div id="IlYAMinutesAFP15" className="text-xl font-sans text-[#6b6d78]">
@@ -101,7 +102,7 @@ return (
                     arcu quis turpis ultrices elementum.{" "}
                 </div>
             </div>
-            <img src="https://file.rendit.io/n/moyNPGA2G7F1DlDtFkpK.png" alt="Image4" id="Image4" className="w-12" />
+            <img src={Logoschneider} alt="Image4" id="Image4" className="w-12" />
         </div>
         <div className="flex flex-col mt-2 gap-1 w-2/5 items-start">
             <div id="IlYAMinutesAFP7" className="text-xl font-sans text-[#6b6d78]">
