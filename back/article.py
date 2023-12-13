14a23d7dc0cb2db3b02c4a84de910b9dea93028e
akia-web <59091758+akia-web@users.noreply.github.com>
2023-12-13 14:51:36 +0100

    back

---
+++ back/article.py
@@ -0,0 +1,71 @@
+import feedparser
+from textblob import TextBlob
+from textblob_fr import PatternTagger, PatternAnalyzer
+
+
+def getSentiment(text):
+
+    if text>0:
+       return 'positif'
+    else:
+        return 'negatif'
+    
+def searchWordInSetence(sentence):
+    search = ["schneider", "bitcoin", "btc", "sika", "cac40", "gold"]
+    for word in search:
+        if word in sentence:
+            return word
+
+
+
+def getarticles():
+    infoArticle = []
+    tableauFluxRss = [
+    'https://www.abcbourse.com/rss/displaynewsrss', 
+    'https://services.lesechos.fr/rss/investir-actualites-valeurs.xml',
+    'https://services.lesechos.fr/rss/budget.xml',
+    'https://services.lesechos.fr/rss/investir-conseils-boursiers.xml',
+    'https://www.abcbourse.com/rss/chroniquesrss',
+    'https://www.abcbourse.com/rss/lastanalysisrss'
+
+    ]
+
+    for entryFlux in tableauFluxRss:
+        news_feed = feedparser.parse(entryFlux)
+        for entry in news_feed.entries:
+            statisques = {
+                'name': '',
+                'totalArticle' : 0,
+                'totalNegatifArticle': 0,
+                'totalPositifArticle': 0,
+                'articles': []
+            }
+            doPush = True
+            isWordInSentence = searchWordInSetence(entry.title.lower())
+            
+            if isWordInSentence and 'description' in entry:
+                if len(infoArticle)>0:
+                    for statisque in infoArticle:
+                        if statisque["name"] == isWordInSentence:
+                            statisques = statisque
+                            doPush = False
+                        else: 
+                            doPush = True    
+                else:
+                    doPush = True            
+
+                blob = TextBlob(entry.description, pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
+                sentiment = getSentiment(blob.sentiment[0])
+                if sentiment == 'positif':
+                    statisques['totalPositifArticle'] += 1
+                else:
+                    statisques['totalNegatifArticle'] += 1
+                statisques['name']= isWordInSentence
+                statisques['totalArticle'] += 1
+                statisques['articles'].append({'title': entry.title, 'description': entry.description, 'link':entry.link})
+                if doPush:
+                    infoArticle.append(statisques)
+                print(infoArticle)
+    return infoArticle
+
+
---
+++ back/server2.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from article import getarticles
+
+app = Flask(__name__)
+
+totalArticle = 0
+totalNegatifArticle = 0
+totalPositifArticle = 0
+
+# Route Flask pour /articles
+@app.route('/articles')
+def get_articles_stats():
+    global totalArticle, totalNegatifArticle, totalPositifArticle
+
+    # Construction de l'objet JSON avec les statistiques
+    result = getarticles()
+
+    # Renvoi de la réponse au format JSON
+    return jsonify(result)
+
+# Exécution de l'application Flask
+if __name__ == '__main__':
+    host = '127.0.0.1'  # Adresse IP locale
+    port = 8080         # Port d'écoute du serveur
+
+    print(f"Le serveur écoute sur {host}:{port}...")
+    app.run(host=host, port=port)
