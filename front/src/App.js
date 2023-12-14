837d3df40882f43eaa7aa6c627ff10dd8f5d3a0d
Noham <72859612+Noh4m@users.noreply.github.com>
2023-12-14 11:46:47 +0100

    route reac in progress 2

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  Home  from "./pages/Home"
 import './App.css';
-import HeroBanner from './components/HeroBanner';
-import Home from './components/Home';
-import Info from './components/Info';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <>
-    <div className="header">
+    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={ <Home /> }/>
+      </Routes>
+    </BrowserRouter>
+    {/* <div className="header">
       <NavBar />
     </div>
     <div className='HeroBanner'>
     <HeroBanner />
+    </div> */}
     </div>
-
-    {/* <div>
-      <Home />
-    </div> */}
-    {/* <div>
-      <Info />
-    </div> */}
-    </>
   );
 }
 
---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import Home from "../components/Home"
+
+export default function Home() {
+  return (
+      <div>
+        <h1>fsefedfs</h1>
+      </div>
+
+  )
+}
